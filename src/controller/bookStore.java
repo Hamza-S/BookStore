6cc26547fbff76d2b44b91c9d435af147fec3d7b
DESKTOP-O7SJBQQ\hamza <hamzabman@gmail.com>
2020-11-21 20:40:09 -0500

    Add test db connection

--- src/controller/bookStore.java
+++ src/controller/bookStore.java
@@ -1,6 +1,9 @@
 package controller;
 
+import model.Books;
 import java.io.IOException;
+import java.sql.SQLException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -27,7 +30,14 @@ public class bookStore extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		System.out.println("test");
 		response.getWriter().append("Served at: ").append(request.getContextPath());
+		try {
+			Books.main(null);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	
 	}
 
 	/**
