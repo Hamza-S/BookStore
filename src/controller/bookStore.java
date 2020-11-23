112ab08d716717e70aa0ee3e6e71d3382dbbcbaf
DESKTOP-O7SJBQQ\hamza <hamzabman@gmail.com>
2020-11-22 22:03:59 -0500

    Add book cards

--- src/controller/bookStore.java
+++ src/controller/bookStore.java
@@ -36,11 +36,35 @@ public class bookStore extends HttpServlet {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 		try {
+			String books = "";
 			Map<String, BookBean> rv = Books.getInstance().getLibrary();
+			BookBean b = null;
 			for (Map.Entry<String, BookBean> i : rv.entrySet()) {
-				System.out.println(i.getValue().getTitle());
+				b = i.getValue();
+				books += "<div class=\"col-md-4\">";
+				books += "<div class=\"card mb-4 shadow-sm\">";
+				books += "<svg class=\"bd-placeholder-img card-img-top\" width=\"100%\" height=\"225\" xmlns=\"http://www.w3.org/2000/svg\"\r\n" + 
+						"preserveAspectRatio=\"xMidYMid slice\" focusable=\"false\" role=\"img\">\r\n" + 
+						"<image href=\"https://upload.wikimedia.org/wikipedia/en/c/c9/Harry_Potter_and_the_Goblet_of_Fire_Poster.jpg\"></image>\r\n" + 
+						"</svg>";
+				books += "<div class=\"card-body\">";
+				books += "<h6 class=\"card-text\"> " + b.getTitle() + "</h6>";
+				books += "<h7 class=\"card-text\"> " + b.getCategory() + "</h7>";
+				books += "<br />";
+				books += "<h7 class=\"card-text\"> $" + b.getPrice() + "</h7>";
+				books += "<br />";
+				books += "<div class=\"btn-group\">";
+				books += "<form action=\"bookStore\" method=\"GET\">";
+				books += "<input type=\"hidden\" name=\"bid\" value=\"" + b.getBid() + "\" />";
+				books += "<button action='submit' class=\"btn btn-sm btn-outline-secondary\" name='moreInfo' value=\"true\">More Info</button>";
+				books += "</form>";
+				books += "</div>";
+				books += "</div>";
+				books += "</div>";
+				books += "</div>";
 
 			}
+			request.getServletContext().setAttribute("library", books);
 		}catch(Exception e) {
 			e.printStackTrace();
 		}
