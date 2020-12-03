9ee9b9e14b5b853b81ffa932da3885efde3cae93
ryang-123 <37124601+ryang-123@users.noreply.github.com>
2020-12-02 20:30:47 -0500

    Added changes to payment to login and register on the page. Updated sql

---
+++ WebContent/res/js/payment.js
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+function showDiv() {
+	document.getElementById('register-div').style.display = "block";
+	document.getElementById('login-div').style.display = "none";
+}
+
+function showDiv2() {
+	document.getElementById('login-div').style.display = "block";
+	document.getElementById('register-div').style.display = "none";
+}
