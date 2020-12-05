6df5568af5a73a565022aa91493ed1f25cb653db
Usama <usama01@my.yorku.ca>
2020-12-04 21:11:11 -0500

    front end pretty

--- WebContent/res/js/register.js
+++ WebContent/res/js/register.js
@@ -14,3 +14,16 @@ function showPassword(){
 		password2.type="password";
 	}
 }
+
+
+function check() {
+	if (document.getElementById('password1').value ==
+	    document.getElementById('password2').value) {
+	    document.getElementById('message').style.color = 'green';
+	    document.getElementById('message').innerHTML = 'matching';
+	  } 
+	  else {
+	    document.getElementById('message').style.color = 'red';
+	    document.getElementById('message').innerHTML = 'not matching';
+	  }
+}
